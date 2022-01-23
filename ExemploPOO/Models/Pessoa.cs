8e5cb93e6bf56d531962e8e28a9ae5d5658f71a4
Inácio Júnior <juniorcosta335@gmail.com>
2022-01-22 23:25:36 -0300

    confirmar

--- ExemploPOO/Models/Pessoa.cs
+++ ExemploPOO/Models/Pessoa.cs
@@ -4,11 +4,11 @@ namespace ExemploPOO.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
 
         public int Idade {get; set; }
 
-        public string Documento { get; set; }
+        public string? Documento { get; set; }
 
         public virtual void Apresentar()
         {
